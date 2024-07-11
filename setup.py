1e6704bea17195a4723d9bbf70dbdb860ed8dcef
Vishnu Prakash <vishnu.p@ship-watch.com>
2024-07-11 14:47:46 +0530

    build: changed repo name

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="vpk",  # Replace with your package name
+    name="vpk_custom_functions",  # Replace with your package name
     version="0.0.1",  # Initial release version
     author="Vishnu Prakash",
     author_email="vishnucheppanam@gmail.com",
     description="Contains custom functions which are used repeatedly during development",
-    url="https://github.com/vishnuprksh/vpk",  # Replace with your package URL
+    url="https://github.com/vishnuprksh/vpk_custom_functions",  # Replace with your package URL
     packages=find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    download_url="https://github.com/vishnuprksh/vpk/archive/refs/tags/0.0.1.tar.gz",
+    download_url="https://github.com/vishnuprksh/vpk_custom_functions/archive/refs/tags/0.0.1.tar.gz",
     install_requires=[            # I get to this in a second
         'IPython',
         'pandas',
